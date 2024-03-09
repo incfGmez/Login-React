384d626e8a7e93f08436a53510230adc51d18359
tuprogamadorfavo <mgomeznavarro.js@gmail.com>
2024-03-09 16:31:20 -0300

    Agregar divs para circulos al icono

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -9,9 +9,17 @@ export const egistro = () => {
         <h1>Crea una cuenta</h1>
         {/* Pasa diferentes iconos como propiedades */}
         <section className="contenedor-iconos">
+
+          <section className="icono">
             <SocialButton icon={faFacebook} />
+          </section>
+          <section className="icono">
             <SocialButton icon={faGithub} />
+          </section>
+          <section className="icono">
             <SocialButton icon={faLinkedin} />
+          </section>
+          
         </section>
         <p>O usa tu email para registrarte</p>
         <Formulario />
